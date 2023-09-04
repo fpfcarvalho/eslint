48adc6727d2937545ef4bb0b873fe1b383fceb93
fpfcarvalho <fpfcarvalho@gmail.com>
2023-09-04 12:14:40 +0100

    estlint array-callback-return

--- src/app/rules/array-callback-return/array-callback-return.component.ts
+++ src/app/rules/array-callback-return/array-callback-return.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-array-callback-return',
   templateUrl: './array-callback-return.component.html',
   styleUrls: ['./array-callback-return.component.scss']
 })
-export class ArrayCallbackReturnComponent {
+export class ArrayCallbackReturnComponent implements OnInit {
 
+  ngOnInit() {
+    const myArray: any[] = [{ prop: 'prop1' }, { prop: 'prop2' }, { prop: 'prop3' }];
+    const undefAllTheThings = myArray.map(item => {
+      item.prop = 'propX';
+    });
+
+    const indexMap = myArray.reduce(function(memo, item, index) {
+      memo[index] = item;
+    }, {});
+
+    const foo = Array.from(myArray, function(node) {
+      if (node.prop === "DIV") {
+        return true;
+      }
+    });
+  }
 }
