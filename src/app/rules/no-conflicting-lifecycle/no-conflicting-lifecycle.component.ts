034b34d545a0e1847cf3db1a540a5517c10f27df
fpfcarvalho <fpfcarvalho@gmail.com>
2023-09-05 17:06:49 +0100

    no-conflicting-lifecycle

--- src/app/rules/no-conflicting-lifecycle/no-conflicting-lifecycle.component.ts
+++ src/app/rules/no-conflicting-lifecycle/no-conflicting-lifecycle.component.ts
@@ -1,13 +1,12 @@
-import { Component, DoCheck, OnChanges } from '@angular/core';
+import { Component, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'exo-no-conflicting-lifecycle',
   templateUrl: './no-conflicting-lifecycle.component.html',
   styleUrls: ['./no-conflicting-lifecycle.component.scss'],
 })
-export class NoConflictingLifecycleComponent implements DoCheck, OnChanges {
+export class NoConflictingLifecycleComponent implements OnChanges {
   test() {}
   test1() {}
-  ngDoCheck() {}
   ngOnChanges() {}
 }
