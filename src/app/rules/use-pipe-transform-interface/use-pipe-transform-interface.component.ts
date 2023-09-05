fcca9517ddde06135c5027bc2b9f52b4eddeeaed
fpfcarvalho <fpfcarvalho@gmail.com>
2023-09-05 23:32:50 +0100

    use-pipe-transform-interface

--- src/app/rules/use-pipe-transform-interface/use-pipe-transform-interface.component.ts
+++ src/app/rules/use-pipe-transform-interface/use-pipe-transform-interface.component.ts
@@ -1,7 +1,7 @@
-import { Pipe } from '@angular/core';
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({ name: 'test' })
-export class UsePipeTransformInterfaceComponent {
+export class UsePipeTransformInterfaceComponent implements PipeTransform {
 
   transform(value: string) {}
 }
