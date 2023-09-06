e37165e9212bb5b911857a17c19d34b3630aa86f
fpfcarvalho <fpfcarvalho@gmail.com>
2023-09-06 08:51:47 +0100

    max-statements

--- src/app/rules/max-statements/max-statements.component.ts
+++ src/app/rules/max-statements/max-statements.component.ts
@@ -8,14 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class MaxStatementsComponent implements OnInit {
 
   ngOnInit() {
-    const xpto1 = 1;
-    const xpto2 = 1;
-    const xpto3 = 1;
+    const xpto1 = 1; const xpto2 = 1; const xpto3 = 1;
     const xpto4 = 1;
     const xpto5 = 1;
     const xpto6 = 1;
     const xpto7 = 1;
     const xpto8 = 1;
+    const xpto9 = 1;
+    const xpto10 = 1;
+    const xpto11 = 1;
+    const xpto12 = 1;
+    const xpto13 = 1;
+    const xpto14 = 1;
   }
 
 }
